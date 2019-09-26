f9438bbe3e0c027db0b97683fa548f9f66424dc9
Purich Sangprasert <purich.sangprasert@mail.kmutt.ac.th>
2019-09-26 15:23:23 +0700

    <Commit> Change Host

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.post("/api/upload",upload.array('excelFile'),(req,res) => {
 
 })
 
-app.listen(process.env.PORT || 6020, '*' , () => {
+app.listen(process.env.PORT || 6020, 'localhost' , () => {
   //isConnectedBlockchain();
   console.log("Server Running at localhost:6020");
 })
